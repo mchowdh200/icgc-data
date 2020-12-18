8ed7b0843e5861cfcc4baa345ee20040d9a3f86b
Murad <mchowdh200@gmail.com>
2020-12-18 11:48:15 -0700

    update to smoove calling procedure.

--- call_smoove.sh
+++ call_smoove.sh
@@ -1,49 +1,94 @@
 #!/bin/bash
 set -eu
 
-function call_svs() {
-    object_id=$(sed 's/-//g' <<<"$1")
-    file_name=$2
-    donor_id=$3
-    specimen_type=$(sed 's/-//g' <<<"$4" | sed 's/\s+/\./g')
-    fasta=$5
-    exclude_bed=$6
-    bam=/mnt/icgc/$object_id/$file_name
+function setup() {
+    local data_dir=$1
+    local manifest=$2
 
-    smoove call \
-        --name $donor_id.$specimen_type \
-        --fasta $fasta \
-        --exclude $exclude_bed \
-        --outdir $donor_id.$specimen_type \
-        --genotype \
-        --duphold
+    # get the header of the manifset
+    head -1 $manifest > $data_dir/manifest_header.tsv
+
+    # get the ref genome
+    aws s3 cp s3://layerlabcu/ref/genomes/hs37d5/hs37d5.fa $data_dir/ref/
+    aws s3 cp s3://layerlabcu/ref/genomes/hs37d5/hs37d5.fa.fai $data_dir/ref/
+
+    # get exclude bed
+    aws s3 cp s3://layerlabcu/BED/ceph18.b37.lumpy.exclude.2014-01-15.bed $data_dir/BED
 }
-export -f call_svs
 
-# get ref genome
-aws s3 cp s3://layerlabcu/ref/genomes/hs37d5/hs37d5.fa /mnt/local/data/ref/
-aws s3 cp s3://layerlabcu/ref/genomes/hs37d5/hs37d5.fa.fai /mnt/local/data/ref/
-fasta=/mnt/local/data/ref/hs37d5.fa
+function download_and_call() {
+    ## Inputs:
+    # 0. base $data_dir
+    # 1. line of manifest to download with
+    # 2. path to the object_donor_specimen.tsv
+    #
+    ## Procedure:
+    # 1. DONE get the $file_name from the manifest line
+    # 2. DONE match the $file_name to the $donor_id/$specimen_type
+    # 3. DONE cat the manifest line to the header to create a "mini-manifest"
+    # 4. DONE create output directory using $donor_id/$specimen_type
+    # 5. DONE use the score client to download file using the mini manifest
+    # 6. DONE call svs with smoove
+    # 7. TODO upload results vcf to s3
+    # 8. TODO clean up working directory
+    
+    local data_dir=$1
+    local manifest_line=$2
+    local donor_table=$3
 
-# get exclude bed
-aws s3 cp s3://layerlabcu/BED/ceph18.b37.lumpy.exclude.2014-01-15.bed /mnt/local/data/BED
-exclude_bed=/mnt/local/data/BED/ceph18.b37.lumpy.exclude.2014-01-15.bed
+    ## get file_name from manifest
+    local file_name=$(cut -f5 <<<"$manifest_line")
 
-# mount icgc bams using manifest
-sudo mkdir /mnt/icgc
-sudo chmod 777 /mnt/icgc
-score-client mount \
-    --mount-point /mnt/icgc \
-    --manifest prostate_cancer_manifest.tsv \
-    --cache-metadata \
-    --daemonize
+    ## match to donor_id/specimen_type
+    local donor_id=$(grep "$file_name" "$donor_table" | grep -v mini | cut -f4)
 
-# call svs with smoove -----
-# format of data list:
-# file_id,object_id,file_name,donor_id,specimen_type
-# {0}    ,{1}      ,{2}      ,{3}     ,{4}
-tail -n+2 object_donor_speciment.csv |
-    gargs -s ',' -p 1 "call_svs {1} {2} {3} {4} $fasta $exclude_bed"
+    # remove hyphen and spaces, sep words with dots
+    local specimen_type=$(grep "$file_name" "$donor_table" | grep -v mini |
+                          cut -f5 | sed 's/-//g' | sed -E 's/\s+/\./g')
 
+    ## create mini-manifest
+    # first create working directory based on the donor_id/specimen_type
+    # then place the mini manifest in the working directory
+    [[ ! -d $data_dir/$donor_id ]] &&
+        mkdir $data_dir/$donor_id
+    [[ ! -d $data_dir/$donor_id/$specimen_type ]] &&
+        mkdir $data_dir/$donor_id/$specimen_type
+    cat $data_dir/manifest_header.tsv <(echo "$manifest_line") \
+        > $data_dir/$donor_id/$specimen_type/manifest.tsv
 
+    ## Download BAM
+    score-client/bin/scoreclient download \
+        --verify-connection false \
+        --validate false \
+        --output-dir $data_dir/$donor_id/$specimen_type \
+        --manifest $data_dir/$donor_id/$specimen_type/manifest.tsv \
 
+    ## Call SVs
+        smoove call \
+            --name $donor_id.$specimen_type \
+            --fasta $data_dir/ref/hs37d5.fa \
+            --exclude $data_dir/BED/ceph18.b37.lumpy.exclude.2014-01-15.bed \
+            --outdir $donor_id/$specimen_type \
+            --genotype \
+            --duphold
+
+    ## Upload results VCF TODO
+    ## Cleanup working directory TODO
+
+}
+
+function testing() {
+    local data_dir=~/Repositories/icgc-data
+    local manifest=$data_dir/prostate_cancer_manifest.tsv
+    local donor_table=$data_dir/object_donor_specimen.tsv
+
+    # get the second line from the manifest for testing
+    local manifest_line=$(head -2 $manifest | tail -1)
+
+    # setup $data_dir $manifest
+
+    download_and_call "$data_dir" "$manifest_line" "$donor_table"
+}
+
+### Run everything
+testing
