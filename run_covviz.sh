05b596be47893236dcfe5b5b26866ab7950728c9
Murad <mchowdh200@gmail.com>
2021-02-26 14:54:26 -0700

    make the snakemake command use sudo so downstream rules can use it.

--- run_covviz.sh
+++ run_covviz.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
-snakemake -s rules/covviz.smk \
-          --configfile conf/covviz.yaml \
-          -j $(grep -c ^processor /proc/cpuinfo)
+sudo snakemake -s rules/covviz.smk \
+               --configfile conf/covviz.yaml \
+               -j $(grep -c ^processor /proc/cpuinfo)
