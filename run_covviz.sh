d558dce2b487b3e44d2cd9c6c5251aa8adf1c08d
Murad <mchowdh200@gmail.com>
2021-02-26 14:57:16 -0700

    can't use sudo...

--- run_covviz.sh
+++ run_covviz.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
-sudo snakemake -s rules/covviz.smk \
-               --configfile conf/covviz.yaml \
-               -j $(grep -c ^processor /proc/cpuinfo)
+snakemake -s rules/covviz.smk \
+          --configfile conf/covviz.yaml \
+          -j $(grep -c ^processor /proc/cpuinfo)
