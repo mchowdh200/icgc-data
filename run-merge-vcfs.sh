e68e777a12e2c966d7625e60347ecee2e500c6e9
Murad <mchowdh200@gmail.com>
2021-03-02 15:32:37 -0700

    no more dry run.

--- run-merge-vcfs.sh
+++ run-merge-vcfs.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-snakemake -n -s rules/merge-vcfs.smk \
+snakemake -s rules/merge-vcfs.smk \
           --configfile conf/merge-vcfs.yaml \
           -j $(grep -c ^processor /proc/cpuinfo) \
           --use-conda
