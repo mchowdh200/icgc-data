3b5887e0458c442916a614cf6009b9cba8ef7283
Murad <mchowdh200@gmail.com>
2021-05-14 13:59:56 -0600

    need to make the smoove DAG solver greedy

--- run_smoove.sh
+++ run_smoove.sh
@@ -3,4 +3,6 @@
 snakemake -s rules/smoove.smk \
           --configfile rules/conf/config.yaml \
           --resources disk_mb=$(df -m | grep /mnt/local | awk '{print $4*0.95}') \
+          --scheduler greedy \
+          --use-conda --conda-frontend mamba
           -j $(grep -c ^processor /proc/cpuinfo)
