5671b7f6b04be7c06fa0c8818a212b09351bb09d
Murad <mchowdh200@gmail.com>
2021-05-17 10:33:52 -0600

    update run script for smoove.

--- run_smoove.sh
+++ run_smoove.sh
@@ -2,6 +2,5 @@
 
 snakemake -s rules/smoove.smk \
           --configfile rules/conf/config.yaml \
-          --resources disk_mb=$(df -m | grep /mnt/local | awk '{print int($4*0.9)}') \
           --use-conda --conda-frontend mamba \
           -j $(grep -c ^processor /proc/cpuinfo)
