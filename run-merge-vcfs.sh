95d8051115dfd4857daf7ce13f49b791b0dab7ad
Murad <mchowdh200@gmail.com>
2021-06-01 12:18:38 -0600

    Update run script for merging.

--- run-merge-vcfs.sh
+++ run-merge-vcfs.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 snakemake -s rules/merge-vcfs.smk \
-          --configfile conf/merge-vcfs.yaml \
+          --configfile rules/conf/config.yaml \
           -j $(grep -c ^processor /proc/cpuinfo) \
           --use-conda \
           --resources num_downloads=4
