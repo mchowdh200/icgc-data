1ce0a21bc2307b65dd9c62c541ad13957b14081f
Murad <mchowdh200@gmail.com>
2021-06-07 14:34:16 -0600

    be more verbose

--- rules/scripts/excord_cmd.sh
+++ rules/scripts/excord_cmd.sh
@@ -10,6 +10,7 @@ samtools view -f66 $bam | head -100000 |
     > $bam.insert-sizes.txt
 disc_dist=$(python3 scripts/get_disc_distance.py $bam.insert-sizes.txt)
 
+echo "running excord with discordant distance = $disc_dist"
 samtools view -b $bam |
     excord --discordantdistance $disc_dist --fasta $fasta /dev/stdin |
     bgzip -c > $output
