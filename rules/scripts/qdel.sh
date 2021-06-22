713fb385075b59fbbd79136992283b8591880fc1
Murad Chowdhury <much8161@mendel.colorado.edu>
2021-06-22 13:10:40 -0600

    put the cd with the stix command (so it is local)

--- rules/scripts/qdel.sh
+++ rules/scripts/qdel.sh
@@ -10,12 +10,9 @@ i="alt_sort_b" # index directory
 
 # TODO seems to be a bug in stix where you need to call it
 # from the directory with the index in it.
-cd $data_dir
-hit=$(stix -d $d -t DEL -s 500 -i $i -l $c:$s-$s -r $c:$e-$e |
+hit=$(cd $data_dir && stix -d 1kg.ped.db -t DEL -s 500 -i alt_sort_b -l $c:$s-$s -r $c:$e-$e |
       tail -n+2 | awk '{print $7+$8}' | paste -sd " " - )
-cd -
 nz=$(echo "$hit" | tr ' ' '\n' | awk '$1>0' | wc -l)
 max=$(echo "$hit" | tr ' ' '\n' | sort -n | tail -n 1)
 total=$( echo $hit | python scripts/sum.py )
-#echo -e "$nz\t$max\t$total\t$hit"
 echo -e "$c\t$s\t$e\t$nz\t$max\t$total"
