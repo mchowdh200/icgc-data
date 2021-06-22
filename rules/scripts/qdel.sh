3e55cfe324e7202756c1d069b852f591bf03d8cc
Murad <mchowdh200@gmail.com>
2021-06-22 12:10:30 -0600

    use stix on my path

--- rules/scripts/qdel.sh
+++ rules/scripts/qdel.sh
@@ -8,7 +8,7 @@ e=$3
 d="/data/stix/1kg/1kg.ped.db" # ped database
 i="/data/stix/1kg/alt_sort_b" # index directory
 
-hit=$(/data/stix/bin/stix -d $d -t DEL -s 500 -i $i -l $c:$s-$s -r $c:$e-$e |
+hit=$(stix -d $d -t DEL -s 500 -i $i -l $c:$s-$s -r $c:$e-$e |
       tail -n+2 | awk '{print $7+$8}' | paste -sd " " - )
 nz=$(echo "$hit" | tr ' ' '\n' | awk '$1>0' | wc -l)
 max=$(echo "$hit" | tr ' ' '\n' | sort -n | tail -n 1)
