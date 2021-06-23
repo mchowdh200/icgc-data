45509a91e34ec93294d38c2da90c03c87d8d0b56
Murad <mchowdh200@gmail.com>
2021-06-23 14:49:11 -0600

    forgot to provide input to awk

--- rules/scripts/threshold_called_regions.sh
+++ rules/scripts/threshold_called_regions.sh
@@ -4,4 +4,4 @@ input=$1
 output=$2
 threshold=$3
 
-awk -v threshold=$threshold '{OFS="\t"; if ($6>threshold) print $0;}' > $output
+awk -v threshold=$threshold '{OFS="\t"; if ($6>threshold) print $0;}' $input > $output
