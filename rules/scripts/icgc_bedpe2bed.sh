6379e47f58eee695122ab6eb6a28919d0bf28f62
Murad <mchowdh200@gmail.com>
2021-06-23 15:12:07 -0600

    improperly passed variable to awk

--- rules/scripts/icgc_bedpe2bed.sh
+++ rules/scripts/icgc_bedpe2bed.sh
@@ -5,4 +5,5 @@ input=$1
 output=$2
 svtype=$3
 
-zcat $input | tail -n+2 | awk '{OFS="\t"; if ($11=="$svtype") print $1,$2,$6,$11;}' > $output
+zcat $input | tail -n+2 |
+    awk -v svtype=$svtype '{OFS="\t"; if ($11==svtype) print $1,$2,$6,$11;}' > $output
