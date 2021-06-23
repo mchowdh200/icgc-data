dcee8ff6eea6d26785dfe16777c6de4da4e734d1
Murad <mchowdh200@gmail.com>
2021-06-23 15:00:58 -0600

    put all on one line?

--- rules/scripts/icgc_bedpe2bed.sh
+++ rules/scripts/icgc_bedpe2bed.sh
@@ -1,10 +1,8 @@
 #!/bin/env bash
 set -exuo pipefail
+
 input=$1
 output=$2
 svtype=$3
 
-zcat $input |
-    tail -n+2 |
-    awk '{OFS="\t"; if ($11=="$svtype") print $1,$2,$6,$11;}' \
-    > $output
+zcat $input | tail -n+2 | awk '{OFS="\t"; if ($11=="$svtype") print $1,$2,$6,$11;}' > $output
