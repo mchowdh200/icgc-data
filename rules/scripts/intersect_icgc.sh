a6740b1f7f0d753df267425c8e05d770fe734fa2
Murad <mchowdh200@gmail.com>
2021-06-23 19:11:54 -0600

    try -u in bedtools intersect

--- rules/scripts/intersect_icgc.sh
+++ rules/scripts/intersect_icgc.sh
@@ -6,4 +6,4 @@ input_bed=$1
 icgc_bed=$2
 output=$3
 
-bedtools intersect -b $input_bed -a $icgc_bed -f 0.9 -r | bedtools sort | uniq > $output
+bedtools intersect -b $input_bed -a $icgc_bed -f 0.9 -r -u | bedtools sort > $output
