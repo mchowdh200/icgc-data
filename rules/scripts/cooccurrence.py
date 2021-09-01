387583a4d02fce1722bfc854376541b65cc8b09a
Murad <mchowdh200@gmail.com>
2021-09-01 16:23:56 -0600

    see difference in number of parameters

--- rules/scripts/cooccurrence.py
+++ rules/scripts/cooccurrence.py
@@ -81,8 +81,10 @@ co_occ.data = compute_pmi(co_occ.row, co_occ.col, co_occ.data, P)
 ### Eliminate elements that are below 2 std above the mean
 mean = np.mean(co_occ.data)
 std = np.std(co_occ.data)
+print(len(co_occ.data))
 co_occ.data[co_occ.data < (mean + 2*(std))] = 0.0
 co_occ.eliminate_zeros()
+print(len(co_occ.data))
 
 
 ### Write results to disk
