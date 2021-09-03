2799572e6dcabd343b64dc55a9663d64039f7fce
Murad <mchowdh200@gmail.com>
2021-09-03 11:20:16 -0600

    just go direct from sparse matrix to graph

--- rules/scripts/co_occ2graph.py
+++ rules/scripts/co_occ2graph.py
@@ -5,15 +5,16 @@ from scipy import sparse
 
 matrix_file = sys.argv[1]
 features_file = sys.argv[2]
-graph = sys.argv[3]
+output_graph = sys.argv[3]
 
 print('loading matrix')
 co_occ = sparse.load_npz(matrix_file)
 
 print('loading features')
 features = [x.rstrip() for x in open(features_file).readlines()]
+node_mapping = {i: f for i, f in enumerate(features)}
 
 ### convert to networkx and write as graphml
-co_occ = pd.DataFrame.sparse.from_spmatrix(data=co_occ, index=features, columns=features)
-G = nx.from_pandas_adjacency(co_occ)
+G = nx.from_scipy_sparse(co_occ)
+relabel_nodes(G, node_mapping, copy=False)
 nx.write_graphml(G, output_graph)
