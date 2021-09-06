a2da0636e303841eea5f9e387fe91398de7cdf19
Murad <mchowdh200@gmail.com>
2021-09-06 16:18:26 -0600

    change gene bed to run analysis on

--- rules/run_sv_analysis.sh
+++ rules/run_sv_analysis.sh
@@ -6,7 +6,7 @@ snakemake -s ~/Repositories/icgc-data/rules/sv_analysis.smk -j 8 \
           --config outdir='/home/murad/data/icgc/sv_analysis' \
           donor_table='/home/murad/Repositories/icgc-data/data_listings/donor_table.tsv' \
           sample_names='/home/murad/Repositories/icgc-data/data_listings/sample_names.txt' \
-          genes_bed='/home/murad/Repositories/icgc-data/data_listings/genes.hg19.collapsed.bed' \
+          genes_bed='/home/murad/Repositories/icgc-data/data_listings/hg19.protein_coding.bed' \
           regions_8p16q='/home/murad/Repositories/icgc-data/data_listings/8p16q_regions.bed' \
           cytoband_bed='/home/murad/Repositories/icgc-data/data_listings/hg37_cytoband.bed' \
           somaticSV_bucket='s3://layerlabcu/icgc/manta-tumour-normal' \
