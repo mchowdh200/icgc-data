3e6eafe571456e4d6e8073e32179e52a93b6a52b
Murad <mchowdh200@gmail.com>
2021-11-03 16:31:17 -0600

    special case for icgc INV

--- rules/scripts/icgc_bedpe2bed.sh
+++ rules/scripts/icgc_bedpe2bed.sh
@@ -5,5 +5,10 @@ input=$1
 output=$2
 svtype=$3
 
-zcat $input | tail -n+2 |
-    awk -v svtype=$svtype '{OFS="\t"; if ($11==svtype) print $1,$2,$6,$11;}' > $output
+if [[ $svtype=='INV' ]]; then
+    zcat $input | tail -n+2 |
+        awk '{OFS="\t"; if ($11=="h2hINV" || $11=="t2tINV") print $1,$2,$6,$11;}' > $output
+else
+    zcat $input | tail -n+2 |
+        awk -v svtype=$svtype '{OFS="\t"; if ($11==svtype) print $1,$2,$6,$11;}' > $output
+fi
