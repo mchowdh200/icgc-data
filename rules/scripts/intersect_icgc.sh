0fe265860a2b98e9fe0da25dc07e7d26f809fe29
Murad <mchowdh200@gmail.com>
2021-11-05 14:41:32 -0600

    filter lines with 0 count overlaps.

--- rules/scripts/intersect_icgc.sh
+++ rules/scripts/intersect_icgc.sh
@@ -6,4 +6,6 @@ input_bed=$1
 icgc_bed=$2
 output=$3
 
-bedtools intersect -c -b $input_bed -a $icgc_bed -f 0.9 -r | bedtools sort > $output
+bedtools intersect -c -b $input_bed -a $icgc_bed -f 0.9 -r |
+    awk '{ if ($5 > 0) print }' |
+    bedtools sort > $output
