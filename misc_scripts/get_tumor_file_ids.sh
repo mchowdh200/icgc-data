2d37e4aa4c96201b2573180c7ec3ac3045b95f33
Murad <mchowdh200@gmail.com>
2022-05-10 10:13:30 -0600

    script to get files in directory associated with tumor fileIDs

--- misc_scripts/get_tumor_file_ids.sh
+++ misc_scripts/get_tumor_file_ids.sh
@@ -1,13 +1,18 @@
 #!/bin/env bash
 
-out_normal=$1
-out_tumor=$2
+set -eu
+
+# pipe list of files that contain icgc file ids and separate into
+# lists by if they are tumor or normal associated
+
+indir=$1
+outdir=$2
 
 donor_table="../data_listings/donor_table.tsv"
 fid2type=$(tail -n+2 $donor_table | cut -f2,7)
-echo "$fid2type" | wc -l
-echo "$fid2type" | grep -i normal > $out_normal
-echo "$fid2type" | grep -i tumour > $out_tumor
-wc -l $out_normal
-wc -l $out_tumor
+normal_ids=$(echo "$fid2type" | grep -i normal | cut -f1 | tr '\n' '|')
+tumor_ids=$(echo "$fid2type" | grep -i tumour | cut -f1 | tr '\n' '|')
+
+ls $indir | grep -i -E \"$normal_ids\" > $outdir/normals.txt
+ls $indir | grep -i -E \"$tumor_ids\" > $outdir/tumors.txt
 
